66832d2f3325f7306e2558caeab236fb68ae244b
platwa <60552000+platwa@users.noreply.github.com>
2020-05-31 23:17:23 +0500

    Add files via upload

--- Spammer.py
+++ Spammer.py
@@ -8,6 +8,28 @@ init()
 
 print(Fore.YELLOW+"GmailSpammer by PLATWA сделано с любовью"+'\n'+Fore.WHITE)
 
+logins = {
+	'1':'Bot2Python@gmail.com',
+	'2':'Bot3Python@gmail.com',
+	'3':'Bot4Python@gmail.com',
+	'4':'Bot5Python@gmail.com',
+	'5':'Bot6Python@gmail.com',
+	'6':'Bot7Python@gmail.com',
+	'7':'Bot8Python@gmail.com',
+	'8':'Bot9Python@gmail.com',
+	'9':'Bot10Python@gmail.com',
+	'10':'Bot11Python@gmail.com',
+	'11':'Bot12Python@gmail.com',
+	'12':'Bot13Python@gmail.com',
+	'13':'Bot14Python@gmail.com',
+	'14':'Bot15Python@gmail.com',
+	'15':'Bot16Python@gmail.com',
+	'16':'Bot17Python@gmail.com',
+	'17':'Bot18Python@gmail.com',
+	'18':'Bot19Python@gmail.com',
+	'19':'Bot20Python@gmail.com',
+	'20':'Bot21Python@gmail.com',
+}
 context = ssl.create_default_context()
 debug = 1
 port = 465  # для SSL подключения
@@ -15,12 +37,12 @@ smtp_server = "smtp.gmail.com"
 def acc():
 	global sender_email
 	global password
-	sender_email = input("Введите gmail Бота. Оставьте поле пустым, чтобы использовать [Bot2Python@gmail.com]: ")
-	if sender_email == '':
-		sender_email = "Bot2Python@gmail.com"  # Ваш емайл
-	password = input("Введите пароль Бота. Оставьте поле пустым, чтобы использовать пароль для [Bot2Python@gmail.com]: ")
-	if password == '':
-		password = 'te155735'
+	sender_email = input("Введите gmail Бота. Введите число от 1 до 20 (работают не все), чтобы использовать стандартных ботов: ")
+	password = 'te155735'
+	if sender_email == '1' or sender_email == '2' or sender_email == '3' or sender_email == '4' or sender_email == '5' or sender_email == '6' or sender_email == '7' or sender_email == '8' or sender_email == '9' or sender_email == '10' or sender_email == '11' or sender_email == '12' or sender_email == '13' or sender_email == '14' or sender_email == '15' or sender_email == '16' or sender_email == '17' or sender_email == '18' or sender_email == '19' or sender_email == '20':
+		sender_email = logins[sender_email]
+	else:
+		password = input("Введите пароль вашего бота: ")
 	try:
 		print('\n'+Fore.BLACK+Style.BRIGHT+"Пробное подключение..."+Fore.WHITE+Style.DIM)
 		with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
